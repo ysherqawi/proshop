4fcedda1a3a9607205a74113d5e7474087bdfb3d
ysherqawi <ysherqawi@gmail.com>
2020-11-21 15:30:14 +0200

    Implementing React Router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p3 rounded'>
-      <a href={`/products/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top'></Card.Img>
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/products/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating
             value={product.rating}
