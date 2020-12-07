901b648e5fc3cfc8c51845cd77b6ed046430a3dc
ysherqawi <ysherqawi@gmail.com>
2020-12-07 03:08:53 +0200

    Fix Product Route

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = find((p) => p._id === req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
