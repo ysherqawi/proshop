e5083c8ce157f9ea228ee7d3c1fe45cc9a6d511a
ysherqawi <ysherqawi@gmail.com>
2021-01-10 05:35:43 +0200

    Fix User Reducers

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -9,35 +9,37 @@ import {
 } from '../actions/types';
 
 export const userLoginReducer = (
-  state = { loading: true, userInfo: {}, error: null },
+  state = { loading: null, userInfo: null, error: null },
   action
 ) => {
   const { type, payload } = action;
   switch (type) {
     case USER_LOGIN_REQUEST:
-      return { ...state };
+      return { ...state, loading: true };
     case USER_LOGIN_SUCCESS:
       return { ...state, loading: false, userInfo: payload };
     case USER_LOGIN_FAIL:
       return { ...state, loading: false, error: payload };
     case USER_LOGOUT:
-      return {};
+      return { ...state, loading: false, userInfo: null, error: null };
     default:
       return state;
   }
 };
 export const userRegisterReducer = (
-  state = { loading: true, userInfo: {}, error: null },
+  state = { loading: null, userInfo: null, error: null },
   action
 ) => {
   const { type, payload } = action;
   switch (type) {
     case USER_REGISTER_REQUEST:
-      return { ...state };
+      return { ...state, loading: true };
     case USER_REGISTER_SUCCESS:
       return { ...state, loading: false, userInfo: payload };
     case USER_REGISTER_FAIL:
       return { ...state, loading: false, error: payload };
+    case USER_LOGOUT:
+      return { ...state, loading: false, userInfo: null, error: null };
     default:
       return state;
   }
