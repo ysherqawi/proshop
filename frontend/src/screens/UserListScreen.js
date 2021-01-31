c1c8991de71eddbec46cd4fd7b931e809dcaf131
ysherqawi <ysherqawi@gmail.com>
2021-01-31 20:01:48 +0200

    UI Delete Handler

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,21 +4,27 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { getUserList } from '../actions/user';
+import { getUserList, deleteUser } from '../actions/user';
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
-  const userList = useSelector((state) => state.userList);
-  const { loading, users, error } = userList;
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+  const userList = useSelector((state) => state.userList);
+  const { loading, users, error } = userList;
+  const userDelete = useSelector((state) => state.userDelete);
+  const {
+    loading: deleteLoading,
+    success: deleteSuccess,
+    error: deleteError,
+  } = userDelete;
 
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) dispatch(getUserList());
     else history.push('/login');
-  }, [dispatch, history]);
+  }, [dispatch, history, deleteSuccess]);
 
   const deleteHandler = (id) => {
-    console.log('delete');
+    if (window.confirm('Are you sure?')) dispatch(deleteUser(id));
   };
 
   return (
@@ -29,53 +35,58 @@ const UserListScreen = ({ history }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Table borderd hover striped responsive variant='dark' size='sm'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Admin</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td>{user._id}</td>
-                <td>
-                  <a href={`mailto:${user.email}`}></a> {user.email}
-                </td>
-                <td>
-                  {user.isAdmin ? (
-                    <i
-                      className='fas fa-check fa-lg'
-                      style={{ color: 'green' }}
-                    ></i>
-                  ) : (
-                    <i
-                      className='fas fa-times fa-lg'
-                      style={{ color: 'red' }}
-                    ></i>
-                  )}
-                </td>
-                <td>
-                  <LinkContainer to={`/users/${user._id}/edit`}>
-                    <Button size='sm' variant='light'>
-                      <i className='fas fa-edit fa-lg' />
-                    </Button>
-                  </LinkContainer>
-                  <Button
-                    size='sm'
-                    variant='danger'
-                    onClick={() => deleteHandler(user._id)}
-                  >
-                    <i className='fas fa-trash fa-lg' />
-                  </Button>
-                </td>
+        <>
+          {deleteLoading && <Loader />}
+          {deleteSuccess && <Message variant='success'>User deleted!</Message>}
+          {deleteError && <Message variant='danger'>{deleteError}</Message>}
+          <Table borderd hover striped responsive variant='dark' size='sm'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Admin</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>
+                    <a href={`mailto:${user.email}`}></a> {user.email}
+                  </td>
+                  <td>
+                    {user.isAdmin ? (
+                      <i
+                        className='fas fa-check fa-lg'
+                        style={{ color: 'green' }}
+                      ></i>
+                    ) : (
+                      <i
+                        className='fas fa-times fa-lg'
+                        style={{ color: 'red' }}
+                      ></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/users/${user._id}/edit`}>
+                      <Button size='sm' variant='light'>
+                        <i className='fas fa-edit fa-lg' />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      size='sm'
+                      variant='danger'
+                      onClick={() => deleteHandler(user._id)}
+                    >
+                      <i className='fas fa-trash fa-lg' />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
       )}
     </>
   );
