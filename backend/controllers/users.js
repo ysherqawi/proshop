dcaf8075161abcbf9296706aac9d08c12db0d544
ysherqawi <ysherqawi@gmail.com>
2021-02-01 14:18:13 +0200

    Get Single User & Update User Endpoints

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,7 +9,42 @@ const getUsers = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, users });
 });
 
-// @desc    Get  user
+// @desc    Get single users
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (!user) return next(new ErrorResponse('User not found', 404));
+
+  res.status(200).json({ success: true, user });
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return next(new ErrorResponse('User not found', 404));
+
+  user.name = req.body.name || user.name;
+  user.email = req.body.email || user.email;
+  user.isAdmin = req.body.isAdmin;
+  // if (req.body.password) user.password = req.body.password;
+
+  const updatedUser = await user.save();
+
+  res.status(200).json({
+    success: true,
+    user: {
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    },
+  });
+});
+
+// @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res, next) => {
@@ -22,4 +57,4 @@ const deleteUser = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, message: 'User removed!' });
 });
 
-export { getUsers, deleteUser };
+export { getUsers, getUser, updateUser, deleteUser };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { getUsers, deleteUser } from '../controllers/users.js';
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/users.js';
 import { protect, isAdmin } from '../middlewares/auth.js';
 
 const router = express();
 
 router.route('/').get(protect, isAdmin, getUsers);
-router.route('/:id').delete(protect, isAdmin, deleteUser);
+router
+  .route('/:id')
+  .get(protect, isAdmin, getUser)
+  .put(protect, isAdmin, updateUser)
+  .delete(protect, isAdmin, deleteUser);
 
 export default router;
